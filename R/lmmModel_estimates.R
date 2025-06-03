248ab216c4f344f494992f94127120cf96d0b75f
RafRomB <rafrombec@gmail.com>
2025-06-03 13:44:15 +0200

    Updated lmmModel_estimates function to correctly report the estimated coefficients and standard errors for both, exponential and Gompertz models.

--- R/lmmModel_estimates.R
+++ R/lmmModel_estimates.R
@@ -5,7 +5,8 @@
 #' 
 #' @param model An object of class "lme" representing the linear mixed-effects model fitted by [`lmmModel()`].
 #' @param robust If TRUE, sandwich-based robust estimators
-#' of the standard error of the regression coefficient estimates by [clubSandwich::conf_int()] are provided.
+#' of the standard error of the regression coefficient estimates by [clubSandwich::conf_int()] are provided. Sandwich-based robust estimators
+#' are only available for exponential growth models ('explme').
 #' @param type Character string specifying which small-sample adjustment should be used when `robust = True`. Available options are "CR0", "CR1", "CR1p", "CR1S", "CR2", or "CR3". 
 #' See "Details" section of [clubSandwich::vcovCR()] for further information.
 #' @details
@@ -37,7 +38,23 @@
 #' lmmModel_estimates(lmm)
 #' @export
 
-lmmModel_estimates <- function(model, robust = FALSE, type = "CR2"){
+lmmModel_estimates <- function(model,
+                               robust = FALSE,
+                               type = "CR2") {
+  
+  UseMethod("lmmModel_estimates")
+}
+
+## lmmModel_estimates.explme method
+#' This is the method for \code{lmmModel_estimates()} generic function to calculate synergy
+#' using the exponential tumor growth model.
+#' @rdname lmmModel_estimates
+#' @method lmmModel_estimates explme
+#' @export
+#' 
+lmmModel_estimates.explme <- function(model,
+                                      robust = FALSE,
+                                      type = "CR2"){
   
   # Get table with coefficients of fixed effects and std errors
   
@@ -52,7 +69,9 @@ lmmModel_estimates <- function(model, robust = FALSE, type = "CR2"){
   
 
   # Build table with fixed effects and residuals estimates
-  dt <- data.frame(matrix(t(tTable), nrow = 1, byrow = TRUE), sqrt(model$modelStruct$reStruct[[1]][1]), model$sigma)
+  dt <- data.frame(matrix(t(tTable), nrow = 1, byrow = TRUE), # Fixed effects and their std. dev.
+                   sqrt(diag(nlme::pdMatrix(model$modelStruct$reStruct[[1]]))), # std. dev. of random effects
+                   model$sigma) # std.dev of residuals
   
   # Name columns according to treatments
   trt_names <- names(model$coefficients$fixed)
@@ -63,6 +82,51 @@ lmmModel_estimates <- function(model, robust = FALSE, type = "CR2"){
   
   colnames(dt) <- c(trt_names,"Combination", "sd_Combination","sd_ranef", "sd_resid")
   
-  rownames(dt) <- "estimate"
   return(dt)
 }
+
+
+## lmmModel_estimates.gompertzlme method
+#' This is the method for \code{lmmModel_estimates()} generic function to calculate synergy
+#' using the Gompertz tumor growth model.
+#' @rdname lmmModel_estimates
+#' @method lmmModel_estimates gompertzlme
+#' @export
+#' 
+lmmModel_estimates.gompertzlme <- function(model,
+                                           robust = FALSE,
+                                           type = "CR2"){
+  
+  if(robust) {
+    warning("Sandwich-based robust estimators are only available for exponential growth models.")
+  }
+  
+  # Get table with coefficients of fixed effects and std errors
+  
+  tTable <- summary(model)$tTable[, c("Value", "Std.Error")]
+  
+  # Build table with fixed effects and residuals estimates
+  dt <- data.frame(matrix(t(tTable), nrow = 1, byrow = TRUE), # Fixed effects and their std. dev.
+                   matrix(sqrt(diag(nlme::pdMatrix(model$modelStruct$reStruct[[1]]))), nrow = 1, byrow = TRUE), # std. dev. of random effects
+                   model$sigma) # std.dev of residuals
+  
+  # Name columns according to treatments
+  trt_names <- names(model$coefficients$fixed)
+  trt_names <- sub("Treatment", replacement = "", trt_names)
+  trt_names_r0 <- trt_names[grepl("r0", trt_names)]
+  trt_names_r0[length(trt_names_r0)] <- "r0.Combination"
+  
+  trt_names_rho <- trt_names[grepl("rho", trt_names)]
+  trt_names_rho[length(trt_names_rho)] <- "rho.Combination"
+  
+  
+  trt_names_r0 <- as.vector(rbind(trt_names_r0, paste0("sd_", trt_names_r0)))
+  trt_names_rho <- as.vector(rbind(trt_names_rho, paste0("sd_", trt_names_rho)))
+  trt_names <- c(trt_names_r0, trt_names_rho)
+  
+  colnames(dt) <- c(trt_names,"sd_r0_ranef", "sd_rho_ranef","sd_resid")
+  
+  return(dt)
+}
+
+
